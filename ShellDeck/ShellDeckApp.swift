4657bd5e952c404529873df084688e06f5b15510
chaogeek <chaogeek@gmail.com>
2026-05-26 10:04:15 +0800

    Initial Commit

---
+++ ShellDeck/ShellDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShellDeckApp.swift
+//  ShellDeck
+//
+//  Created by chaogeek on 2026/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ShellDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
